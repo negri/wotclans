5bc7c30d1d9f5da2c79da6b0c2c140318641bbd6
João Paulo Gonçalves Negri <negrijp@gmail.com>
2020-03-26 16:48:19 -0300

    feat(DiscordBot): creates an option on the "clan" command to return a bare list of only the clan tankers.
    One can call as "!w clan SELVA true true" to return a list of all members of the clan, displaying only their Gamer Tags.

--- WoTClansBot/Commands/ClanCommands.cs
+++ WoTClansBot/Commands/ClanCommands.cs
@@ -294,7 +294,12 @@ namespace Negri.Wot.Bot
 
         [Command("clan")]
         [Description("A quick overview of a clan")]
-        public async Task Clan(CommandContext ctx, [Description("The clan **tag**")] string clanTag, [Description("Put `true` to dump all members of the clan")]bool all = false)
+        public async Task Clan(CommandContext ctx,
+            [Description("The clan **tag**")] string clanTag,
+            [Description("Put `true` to dump all members of the clan")]
+            bool all = false,
+            [Description("Put `true` to dump a bare list, with only the players names")]
+            bool bare = false)
         {
             if (!await CanExecute(ctx, Features.Clans))
             {
@@ -339,79 +344,116 @@ namespace Negri.Wot.Bot
                 }
             }
 
-            var sb = new StringBuilder();
-
-            sb.Append($"Information about the `{clan.ClanTag}`");
-            if (!string.IsNullOrWhiteSpace(clan.Country))
-            {
-                sb.Append($" ({clan.Country.ToUpperInvariant()})");
-            }
-
-            sb.AppendLine($", on the {clan.Plataform}, {ctx.User.Mention}:");
-
-            if (!clan.Enabled)
-            {
-                sb.AppendLine($"Data collection for this clan is **disabled**, the reason for this is {clan.DisabledReason}.");
-            }
-
-            sb.AppendLine();
-
-            sb.AppendLine($"Active Members: {clan.Active}; Total Members: {clan.Count};");
-            sb.AppendLine($"Recent Win Rate: {clan.ActiveWinRate:P1}; Overall Win Rate: {clan.TotalWinRate:P1};");
-            sb.AppendLine($"Recent WN8t15: {clan.Top15Wn8:N0}; Overall WN8: {clan.TotalWn8:N0};");
-            if (clan.DeltaDayTop15Wn8.HasValue)
-            {
-                sb.AppendLine($"WN8t15 Variation from 1 day ago: {clan.DeltaDayTop15Wn8.Value:N0}");
-            }
-
-            if (clan.DeltaWeekTop15Wn8.HasValue)
-            {
-                sb.AppendLine($"WN8t15 Variation from 1 week ago: {clan.DeltaWeekTop15Wn8.Value:N0}");
-            }
-
-            if (clan.DeltaMonthTop15Wn8.HasValue)
-            {
-                sb.AppendLine($"WN8t15 Variation from 1 month ago: {clan.DeltaMonthTop15Wn8.Value:N0}");
-            }
-
-            sb.AppendLine($"Recent Actives Battles: {clan.ActiveBattles:N0}; Recent Avg Tier: {clan.ActiveAvgTier:N1}");
-
-            sb.AppendLine();
-            sb.AppendLine("**Top 15 Active Players**");
-            foreach (var p in clan.Top15Players)
-            {
-                sb.AppendLine(
-                    $"{Formatter.MaskedUrl(p.Name, new Uri(p.PlayerOverallUrl))}, {p.MonthBattles} battles, WN8: {p.MonthWn8:N0}");
-            }
-
-            var title = clan.ClanTag.EqualsCiAi(clan.Name) ? clan.ClanTag : $"{clan.ClanTag} - {clan.Name}";
-
-            var color = clan.Top15Wn8.ToColor();
-
             var platformPrefix = clan.Plataform == Platform.PS ? "ps." : string.Empty;
 
-            var embed = new DiscordEmbedBuilder
+            var sb = new StringBuilder();
+
+            if (bare)
             {
-                Title = title,
-                Description = sb.ToString(),
-                Color = new DiscordColor(color.R, color.G, color.B),
-                Url = $"https://{platformPrefix}wotclans.com.br/Clan/{clan.ClanTag}",
-                Author = new DiscordEmbedBuilder.EmbedAuthor
+                if (!clan.Enabled)
                 {
-                    Name = "WoTClans",
-                    Url = $"https://{platform}wotclans.com.br"
-                },
-                Footer = new DiscordEmbedBuilder.EmbedFooter
-                {
-                    Text = $"Data calculated at {clan.Moment:yyyy-MM-dd HH:mm} UTC."
+                    sb.AppendLine($"Data collection for `{clan.ClanTag}` is **disabled**, the reason for this is {clan.DisabledReason}.");
+                    await ctx.RespondAsync(sb.ToString());
+                    return;
                 }
-            };
 
-            Log.Debug($"Returned {nameof(Clan)}({clan.Plataform}.{clan.ClanTag})");
+                var players = all
+                    ? clan.Players.OrderBy(p => p.Name).Select(p => p.Name).ToList()
+                    : clan.Top15Players.Select(p => p.Name).ToList();
 
-            await ctx.RespondAsync("", embed: embed);
+                sb.Append($"{(all ? $"All {players.Count}" : "Top 15")} Tankers on `{clan.ClanTag}`");
+                if (!string.IsNullOrWhiteSpace(clan.Country))
+                {
+                    sb.Append($" ({clan.Country.ToUpperInvariant()})");
+                }
+                sb.AppendLine($", on the {clan.Plataform}, {ctx.User.Mention}:");
+                
+                sb.AppendLine("```");
 
-            if (all)
+                
+                    
+
+                foreach (var gamerTag in players)
+                {
+                    sb.AppendLine(gamerTag);
+                }
+                sb.AppendLine("```");
+                await ctx.RespondAsync(sb.ToString());
+            }
+            else
+            {
+                sb.Append($"Information about `{clan.ClanTag}`");
+                if (!string.IsNullOrWhiteSpace(clan.Country))
+                {
+                    sb.Append($" ({clan.Country.ToUpperInvariant()})");
+                }
+                sb.AppendLine($", on the {clan.Plataform}, {ctx.User.Mention}:");
+
+                if (!clan.Enabled)
+                {
+                    sb.AppendLine(
+                        $"Data collection for this clan is **disabled**, the reason for this is {clan.DisabledReason}.");
+                }
+
+                sb.AppendLine();
+
+                sb.AppendLine($"Active Members: {clan.Active}; Total Members: {clan.Count};");
+                sb.AppendLine($"Recent Win Rate: {clan.ActiveWinRate:P1}; Overall Win Rate: {clan.TotalWinRate:P1};");
+                sb.AppendLine($"Recent WN8t15: {clan.Top15Wn8:N0}; Overall WN8: {clan.TotalWn8:N0};");
+                if (clan.DeltaDayTop15Wn8.HasValue)
+                {
+                    sb.AppendLine($"WN8t15 Variation from 1 day ago: {clan.DeltaDayTop15Wn8.Value:N0}");
+                }
+
+                if (clan.DeltaWeekTop15Wn8.HasValue)
+                {
+                    sb.AppendLine($"WN8t15 Variation from 1 week ago: {clan.DeltaWeekTop15Wn8.Value:N0}");
+                }
+
+                if (clan.DeltaMonthTop15Wn8.HasValue)
+                {
+                    sb.AppendLine($"WN8t15 Variation from 1 month ago: {clan.DeltaMonthTop15Wn8.Value:N0}");
+                }
+
+                sb.AppendLine(
+                    $"Recent Actives Battles: {clan.ActiveBattles:N0}; Recent Avg Tier: {clan.ActiveAvgTier:N1}");
+
+                sb.AppendLine();
+                sb.AppendLine("**Top 15 Active Players**");
+                foreach (var p in clan.Top15Players)
+                {
+                    sb.AppendLine(
+                        $"{Formatter.MaskedUrl(p.Name, new Uri(p.PlayerOverallUrl))}, {p.MonthBattles} battles, WN8: {p.MonthWn8:N0}");
+                }
+
+                var title = clan.ClanTag.EqualsCiAi(clan.Name) ? clan.ClanTag : $"{clan.ClanTag} - {clan.Name}";
+
+                var color = clan.Top15Wn8.ToColor();
+
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = title,
+                    Description = sb.ToString(),
+                    Color = new DiscordColor(color.R, color.G, color.B),
+                    Url = $"https://{platformPrefix}wotclans.com.br/Clan/{clan.ClanTag}",
+                    Author = new DiscordEmbedBuilder.EmbedAuthor
+                    {
+                        Name = "WoTClans",
+                        Url = $"https://{platform}wotclans.com.br"
+                    },
+                    Footer = new DiscordEmbedBuilder.EmbedFooter
+                    {
+                        Text = $"Data calculated at {clan.Moment:yyyy-MM-dd HH:mm} UTC."
+                    }
+                };
+
+                Log.Debug($"Returned {nameof(Clan)}({clan.Plataform}.{clan.ClanTag})");
+
+                await ctx.RespondAsync("", embed: embed);
+            }
+
+
+            if (all & !bare)
             {
                 // We need more responses, as discord limits the amount of data we can send on one message
                 
@@ -419,13 +461,13 @@ namespace Negri.Wot.Bot
                 const int pageSize = 15;
                 var pages = allPlayers.Length / pageSize + 1;
 
-                color = clan.TotalWn8.ToColor();
+                var color = clan.TotalWn8.ToColor();
 
                 for (var currentPage = 0; currentPage < pages; ++currentPage)
                 {
                     sb.Clear();
 
-                    title = (clan.ClanTag.EqualsCiAi(clan.Name) ? clan.ClanTag : $"{clan.ClanTag} - {clan.Name}") +
+                    var title = (clan.ClanTag.EqualsCiAi(clan.Name) ? clan.ClanTag : $"{clan.ClanTag} - {clan.Name}") +
                             $" - Page {currentPage + 1} of {pages}";
 
                     sb.Append($"All members of the `{clan.ClanTag}`");
@@ -442,7 +484,7 @@ namespace Negri.Wot.Bot
                         sb.AppendLine($"{Formatter.MaskedUrl(p.Name, new Uri(p.PlayerOverallUrl))}, {p.MonthBattles} battles, WN8: {p.MonthWn8:N0}");
                     }
 
-                    embed = new DiscordEmbedBuilder
+                    var embed = new DiscordEmbedBuilder
                     {
                         Title = title,
                         Description = sb.ToString(),
